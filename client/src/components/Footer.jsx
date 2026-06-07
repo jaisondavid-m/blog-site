c32801f3e3d4cb8622c2e2111fce8910ca143894
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-06-07 21:15:53 +0530

    fixed error in navbar export

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -70,3 +70,5 @@ function Footer() {
         </footer>
     )
 }
+
+export default Footer
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from "react"
-import NavBar from "../components/NavBar.jsx"
 
 function Home() {
     return (
         <div>
-            <NavBar/>
             <h1 className="text-center p-10">Home Page</h1>
         </div>
     )
