43270deb06d7dd1dee480d5efcdfe47e23d6593d
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-07-25 15:02:06 +0530

    fixed base url

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -22,7 +22,7 @@ import {
 
 import { useAuth } from "../context/AuthContext.jsx"
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+const BASE_URL = "https://json.hackclub.app"
 
 function PostPage() {
 
